8d488fe4feb0d231ded3c91703a99ee211c72039
wuwei1 <wuwei1@ut.cn>
2022-01-26 15:03:02 +0800

    Initial commit

---
+++ src/main/java/com/vivek/wo/crawlerbook/CrawlerBookApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.wo.crawlerbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivek/wo/crawlerbook/CrawlerBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.wo.crawlerbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
